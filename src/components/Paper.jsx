f9e54a8ace74e929ac30f17de0c3eedf2ebb32a2
Luis J. Montes <luisjavm3@gmail.com>
2021-06-09 17:11:33 -0500

    code Paper styles

--- src/components/Paper.jsx
+++ src/components/Paper.jsx
@@ -1,7 +1,11 @@
 import React from 'react';
 
-const Paper = ({ children }) => {
-  return <div></div>;
+const Paper = ({ children, width = '500px', height = '400px' }) => {
+  return (
+    <div className="paper" style={{ width: width, height: height }}>
+      {children}
+    </div>
+  );
 };
 
 export default Paper;
--- src/views/Login.jsx
+++ src/views/Login.jsx
@@ -1,9 +1,14 @@
 import React from 'react';
+import Paper from '../components/Paper';
 
 const Login = () => {
   return (
     <div className="login">
-      <div className="frame frame--padding"></div>
+      <div className="frame frame--padding">
+        <Paper>
+          <p>Lorem ipsum dolor sit.</p>
+        </Paper>
+      </div>
     </div>
   );
 };
