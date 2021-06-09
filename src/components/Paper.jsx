08291cbecddfc5f48b34b3097d395500d9f25ede
Luis J. Montes <luisjavm3@gmail.com>
2021-06-09 16:28:16 -0500

    code Paper component

---
+++ src/components/Paper.jsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const Paper = ({ children }) => {
+  return <div></div>;
+};
+
+export default Paper;
--- src/views/Login.jsx
+++ src/views/Login.jsx
@@ -2,8 +2,8 @@ import React from 'react';
 
 const Login = () => {
   return (
-    <div>
-      <h1>Login</h1>
+    <div className="login">
+      <div className="frame frame--padding"></div>
     </div>
   );
 };
