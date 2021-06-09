60aa679e32e303d59fbdbbec9c665bdf07b4b7d0
Luis J. Montes <luisjavm3@gmail.com>
2021-06-09 14:00:43 -0500

    Create last two PrivateAppRoutes

--- src/routers/PrivateAppRoutes.jsx
+++ src/routers/PrivateAppRoutes.jsx
@@ -1,10 +1,23 @@
 import React from 'react';
+import { Route, Switch } from 'react-router';
+
+import Header from '../components/Header';
+import Todos from '../views/Todos';
+import UserDetails from '../views/UserDetails';
+import UsersList from '../views/UsersList';
 
 const PrivateAppRoutes = () => {
   return (
-    <div>
-      <h1>Private App Routes</h1>
-    </div>
+    <React.Fragment>
+      <Header />
+      <div className="frame frame--padding">
+        <Switch>
+          <Route exact path="/app" component={Todos} />
+          <Route exact path="/app/userdetails" component={UserDetails} />
+          <Route exact path="/app/userslist" component={UsersList} />
+        </Switch>
+      </div>
+    </React.Fragment>
   );
 };
 
