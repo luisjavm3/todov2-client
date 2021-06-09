287afe908e6a60878e190587fc2f9388f73d9f1d
Luis J. Montes <luisjavm3@gmail.com>
2021-06-09 10:26:28 -0500

    Add some views

---
+++ src/routers/AppRouter.jsx
@@ -0,0 +1,15 @@
+import { BrowserRouter, Switch, Route } from 'react-router-dom';
+
+import React from 'react';
+
+const AppRouter = () => {
+  return (
+    <BrowserRouter>
+      <div>
+        <Switch></Switch>
+      </div>
+    </BrowserRouter>
+  );
+};
+
+export default AppRouter;
