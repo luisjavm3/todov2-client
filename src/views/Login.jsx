c0ef553b28877bb4cdcfad3d6441edcb80884fdc
Luis J. Montes <luisjavm3@gmail.com>
2021-06-10 08:24:30 -0500

    install axios

--- src/views/Login.jsx
+++ src/views/Login.jsx
@@ -5,9 +5,21 @@ const Login = () => {
   return (
     <div className="login">
       <div className="frame frame--padding">
-        <Paper>
-          <p>Lorem ipsum dolor sit.</p>
+        {/* <div className="login__paper-wrapper"> */}
+        <Paper className="login__paper">
+          <h2 className="login__title">Login</h2>
+          <form className="login__form">
+            <div className="login__form-group">
+              <label htmlFor="username">username</label>
+              <input type="text" id="username" />
+            </div>
+            <div className="login__form-group">
+              <label htmlFor="password">password</label>
+              <input type="text" id="password" />
+            </div>
+          </form>
         </Paper>
+        {/* </div> */}
       </div>
     </div>
   );
